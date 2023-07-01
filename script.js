d0e1eb1f2aca1951ed337cecb9e07df475416a56
JamieYau <jamie.yau2002@me.com>
2023-07-01 20:58:59 +0100

    Add function that displays all books

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let myLibrary = [new Book("book1", "author1", 100, true)];
+let myLibrary = [
+  new Book("book1", "author1", 100, true),
+  new Book("book2", "author2", 400, false),
+  new Book("book3", "author3", 120, true),
+  new Book("book4", "author4", 200, false),];
 
 function Book(title, author, pages, isRead) {
   this.title = title;
@@ -25,18 +29,63 @@ function addBookToLibrary() {
 
   // Add book to library
   myLibrary.push(newBook);
-
-  console.log(myLibrary);
 }
 
 function displayLibrary() {
+  const bookGrid = document.getElementById("book-grid");
 
+  myLibrary.forEach((book) => {
+    const newBook = document.createElement("div");
+    newBook.classList.add("book-card");
+
+    const newBookInfo = document.createElement("div");
+    newBookInfo.classList.add("book-info");
+
+    const newBookActions = document.createElement("div");
+    newBookActions.classList.add("book-actions");
+
+    const createBookElement = (tagName, className, textContent) => {
+      const element = document.createElement(tagName);
+      element.classList.add(className);
+      element.textContent = textContent;
+      return element;
+    };
+
+    const newBookTitle = createBookElement("div", "book-title", book.title);
+    const newBookAuthor = createBookElement("div", "book-author", book.author);
+    const newBookPages = createBookElement("div", "book-pages", book.pages);
+    const newBookReadStatus = createBookElement(
+      "div",
+      "book-read-status",
+      book.isRead ? "Read" : "Not Read"
+    );
+
+    const newBookReadStatusButton = createBookElement(
+      "button",
+      "book-read-status-button",
+      "Change Read Status"
+    );
+    const newBookDeleteButton = createBookElement(
+      "button",
+      "book-delete-button",
+      "Delete Book"
+    );
+
+    newBookInfo.appendChild(newBookTitle);
+    newBookInfo.appendChild(newBookAuthor);
+    newBookInfo.appendChild(newBookPages);
+    newBookInfo.appendChild(newBookReadStatus);
+    newBookActions.appendChild(newBookReadStatusButton);
+    newBookActions.appendChild(newBookDeleteButton);
+
+    newBook.appendChild(newBookInfo);
+    newBook.appendChild(newBookActions);
+
+    bookGrid.appendChild(newBook);
+  });
 }
-
-console.log(myLibrary);
-//addBookToLibrary();
 
 // event listener that will call addBookToLibrary() when button is clicked
 const addBookButton = document.getElementById("add-book-btn");
 addBookButton.addEventListener("click", addBookToLibrary);
-console.log(addBookButton);
+displayLibrary();
